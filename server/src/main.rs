14eea5a1008ab685c62cb69088c0665e8c2b431a
mattuu0 <tomatomato170@gmail.com>
2026-01-22 19:37:45 +0900

    add: websocket サーバーとクライアントを追加

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,20 +1,50 @@
-use actix_web::{App, HttpServer, Responder, get, middleware::Logger, web};
+use actix_web::{App, Error, HttpRequest, HttpResponse, HttpServer, middleware::Logger, rt, web};
+use actix_ws::AggregatedMessage;
+use futures_util::StreamExt as _;
 
-#[get("/")]
-async fn index() -> impl Responder {
-    "Hello, World!"
-}
+async fn echo(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let (res, mut session, stream) = actix_ws::handle(&req, stream)?;
 
-#[get("/{name}")]
-async fn hello(name: web::Path<String>) -> impl Responder {
-    format!("Hello {}!", &name)
+    let mut stream = stream
+        .aggregate_continuations()
+        // aggregate continuation frames up to 1MiB
+        .max_continuation_size(2_usize.pow(20));
+
+    // start task but don't wait for it
+    rt::spawn(async move {
+        // receive messages from websocket
+        while let Some(msg) = stream.next().await {
+            match msg {
+                Ok(AggregatedMessage::Text(text)) => {
+                    println!("{}",text);
+                    // echo text message
+                    session.text(text).await.unwrap();
+                }
+
+                Ok(AggregatedMessage::Binary(bin)) => {
+                    // echo binary message
+                    session.binary(bin).await.unwrap();
+                }
+
+                Ok(AggregatedMessage::Ping(msg)) => {
+                    // respond to PING frame with PONG frame
+                    session.pong(&msg).await.unwrap();
+                }
+
+                _ => {}
+            }
+        }
+    });
+
+    // respond immediately with response connected to WS session
+    Ok(res)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    HttpServer::new(|| App::new().service(index).service(hello).wrap(Logger::default()))
+    HttpServer::new(|| App::new().route("/echo", web::get().to(echo)).wrap(Logger::default()))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
