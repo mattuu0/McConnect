ed75812b7d4400a90d5e73756d63fbe410f49650
mattuu0 <tomatomato170@gmail.com>
2026-01-22 15:01:27 +0900

    actix-web を追加

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, Responder, get, middleware::Logger, web};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello, World!"
+}
+
+#[get("/{name}")]
+async fn hello(name: web::Path<String>) -> impl Responder {
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    HttpServer::new(|| App::new().service(index).service(hello).wrap(Logger::default()))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
